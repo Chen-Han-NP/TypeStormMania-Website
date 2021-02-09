db0ad3f4148990c41b6490e4f8138eacc7c7ca44
TzeSiong <s10204388@connect.np.edu.sg>
2021-02-09 17:07:41 +0800

    Added feature to Leaderboard.js to show leaderboards from api

--- phaser/LeaderBoard.js
+++ phaser/LeaderBoard.js
@@ -4,15 +4,47 @@ class LeaderBoard extends Phaser.Scene {
     }
 
     create(){
-        this.header = this.add.text(0,0,"Leaderboard ", {font: "50px Impact"});
+        
+        console.log(leaderboardData);
+        if(leaderboardData == null){
+            this.notReady = this.add.text(120,500,"Data not loaded, please try again", { fontSize: 20, fill: 'white'})
+            this.header = this.add.text(0,0,"Leaderboard ", {font: "50px Impact"});
 
-        this.backButton = this.add.text(600, 25, 'Go back', { fontSize: 30, fill: 'white', backgroundColor:"green" });
-        this.backButton.setInteractive();
-        this.backButton.setX(game.config.width - this.backButton.width - 25); //Make sure that its alw center
-        this.backButton.on('pointerdown', () => { this.scene.start("MainMenu") });
+            this.backButton = this.add.text(600, 25, 'Go back', { fontSize: 30, fill: 'white', backgroundColor:"green" });
+            this.backButton.setInteractive();
+            this.backButton.setX(game.config.width - this.backButton.width - 25); //Make sure that its alw center
+            this.backButton.on('pointerdown', () => { this.scene.start("MainMenu") });
+        }
 
-        this.rank1 = this.add.text(200,200, "1. ", { fontSize: 30, fill: 'white'});
-        this.rank2 = this.add.text(0,0, "2. ");
+        if (leaderboardData != null){
+            this.header = this.add.text(0,0,"Leaderboard ", {font: "50px Impact"});
+
+            this.backButton = this.add.text(600, 25, 'Go back', { fontSize: 30, fill: 'white', backgroundColor:"green" });
+            this.backButton.setInteractive();
+            this.backButton.setX(game.config.width - this.backButton.width - 25); //Make sure that its alw center
+            this.backButton.on('pointerdown', () => { this.scene.start("MainMenu") });
+            
+            this.statsHeader = this.add.text(100, 200,"RANK", {font: "32px Palatino"});
+            this.statsHeader = this.add.text(220, 200,"NAME", {font: "32px Palatino"});
+            this.statsHeader = this.add.text(450, 200,"SCORE", {font: "32px Palatino"});
+            this.statsHeader = this.add.text(570, 200,"DATE", {font: "32px Palatino"});
+            
+            //TO BE CHANGED
+            var scoreList = [];
+            for (var i = 0; i < leaderboardData.length; i++){
+                scoreList.push(leaderboardData[i].score);
+            }
+            scoreList.sort();
+            console.log(scoreList);
+            leaderboardData.sort();
+            for (var i = 0; i < leaderboardData.length; i++){
+                this.stats = this.add.text(220,230 + i * 26,`${leaderboardData[i].name}`, {font: "26px Calibri"});
+                this.stats = this.add.text(450,230 + i * 26,`${leaderboardData[i].score}`, {font: "26px Calibri"});
+                this.stats = this.add.text(570,230 + i * 26,`${leaderboardData[i].dateOfScore}`, {font: "18px Calibri"});
+            }
+            
+        };
+        
     }
     
 
--- phaser/restDbAPI.js
+++ phaser/restDbAPI.js
@@ -1,5 +1,6 @@
 
     const APIKEY = "602135463f9eb665a16892a6";
+    var leaderboardData;
 
     //get data from restDb
     function getData(){
@@ -19,30 +20,35 @@
         //Display data retrieved from restDb
         $.ajax(setting).done(function (response) {
         
-            let content = "";
+            leaderboardData = response;
     
-            for (var i = 0; i < response.length; i++) {
-                /*
-                content = `${content}<tr id='${response[i]._id}'><td>${response[i].rank}</td>
-                <td>${response[i].name}</td>
-                <td>${response[i].score}</td>
-                <td>${response[i].dateTimeOfScore}
-                <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' 
-                class='update' data-id='${response[i]._id}' data-rank='${response[i].rank}' data-name='${response[i].name}' 
-                data-score='${response[i].score}' data-dateTimeOfScore='${response[i].dateTimeOfScore}'>Update</a></td></tr>`;
-                */
-                console.log("Name: ", response[i].name);
-                console.log("Score: ", response[i].score);
-                console.log("Date time: ", response[i].dateTimeOfScore);
-                content = response[i].name + "," + response[i].score;
-            }
-
+            // for (var i = 0; i < response.length; i++) {
+            //     /*
+            //     content = `${content}<tr id='${response[i]._id}'><td>${response[i].rank}</td>
+            //     <td>${response[i].name}</td>
+            //     <td>${response[i].score}</td>
+            //     <td>${response[i].dateTimeOfScore}
+            //     <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' 
+            //     class='update' data-id='${response[i]._id}' data-rank='${response[i].rank}' data-name='${response[i].name}' 
+            //     data-score='${response[i].score}' data-dateTimeOfScore='${response[i].dateTimeOfScore}'>Update</a></td></tr>`;
+            //     */
+            //     console.log("Name: ", response[i].name);
+            //     console.log("Score: ", response[i].score);
+            //     console.log("Date time: ", response[i].dateTimeOfScore);
+            //     content = response[i].name + "," + response[i].score + "," + response[i].dateTimeOfScore + "," + response[i].rank;
+                
+            // }
+            /*
             $("#leaderboard tbody").html(content);
-            $("#leaderboardList").html(response.length);
+            $("#leaderboardList").html(response.length);*/
       
         });
 
         
     }
     getData();
+    setTimeout(() => {
+        console.log(leaderboardData);
+    }, 3500);
+    
 
