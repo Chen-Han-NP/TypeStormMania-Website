d8cdf90d9716387518f9823e9d5b3cd54367ce5d
TzeSiong <s10204388@connect.np.edu.sg>
2021-02-15 16:42:41 +0800

    Leaderboard data is now sorted successfully

--- phaser/LeaderBoard.js
+++ phaser/LeaderBoard.js
@@ -3,48 +3,65 @@ class LeaderBoard extends Phaser.Scene {
         super({key:"LeaderBoard"});
     }
 
+    preload(){
+        this.load.image('background', '/assets/space3.png');
+    }
+
     create(){
         
-        console.log(leaderboardData);
         if(leaderboardData == null){
             this.notReady = this.add.text(120,500,"Data not loaded, please try again", { fontSize: 20, fill: 'white'})
-            this.header = this.add.text(0,0,"Leaderboard ", {font: "50px Impact"});
+            this.header = this.add.text(250,100,"Leaderboard ", {font: "50px Impact"});
 
-            this.backButton = this.add.text(600, 25, 'Go back', { fontSize: 30, fill: 'white', backgroundColor:"green" });
+            this.backButton = this.add.text(600, 25, '<< Go back', {font:"30px Staatliches", fontStyle:"cursive", fill: 'white', backgroundColor:"green" });
             this.backButton.setInteractive();
             this.backButton.setX(game.config.width - this.backButton.width - 25); //Make sure that its alw center
             this.backButton.on('pointerdown', () => { this.scene.start("MainMenu") });
         }
 
         if (leaderboardData != null){
-            this.header = this.add.text(0,0,"Leaderboard ", {font: "50px Impact"});
+            this.header = this.add.text(250,100,"Leaderboard ", {font: "50px Impact"});
 
-            this.backButton = this.add.text(600, 25, 'Go back', { fontSize: 30, fill: 'white', backgroundColor:"green" });
+            this.backButton = this.add.text(600, 25, '<< Go back', {font:"30px Staatliches", fontStyle:"cursive", fill: 'white', backgroundColor:"green" });
             this.backButton.setInteractive();
             this.backButton.setX(game.config.width - this.backButton.width - 25); //Make sure that its alw center
             this.backButton.on('pointerdown', () => { this.scene.start("MainMenu") });
             
-            this.statsHeader = this.add.text(100, 200,"RANK", {font: "32px Palatino"});
-            this.statsHeader = this.add.text(220, 200,"NAME", {font: "32px Palatino"});
-            this.statsHeader = this.add.text(450, 200,"SCORE", {font: "32px Palatino"});
-            this.statsHeader = this.add.text(570, 200,"DATE", {font: "32px Palatino"});
-            
-            //TO BE CHANGED
+            this.statsHeader = this.add.text(100, 200,"RANK", {font: "32px Dosis"});
+            this.statsHeader = this.add.text(220, 200,"NAME", {font: "32px Dosis"});
+            this.statsHeader = this.add.text(430, 200,"SCORE", {font: "32px Dosis"});
+            this.statsHeader = this.add.text(550, 200,"DATE", {font: "32px Dosis"});
+     
             var scoreList = [];
             for (var i = 0; i < leaderboardData.length; i++){
                 scoreList.push(leaderboardData[i].score);
             }
-            scoreList.sort();
-            console.log(scoreList);
-            leaderboardData.sort();
-            for (var i = 0; i < leaderboardData.length; i++){
-                this.stats = this.add.text(220,230 + i * 26,`${leaderboardData[i].name}`, {font: "26px Calibri"});
-                this.stats = this.add.text(450,230 + i * 26,`${leaderboardData[i].score}`, {font: "26px Calibri"});
-                this.stats = this.add.text(570,230 + i * 26,`${leaderboardData[i].dateOfScore}`, {font: "18px Calibri"});
+
+            scoreList.sort(function(a, b){return a-b}).reverse();
+            //arranged properly
+
+            var arrangedData = [];
+
+            for (var e = 0; e < scoreList.length; e++){
+                for (var f = 0; f < leaderboardData.length; f++){
+
+                    if(scoreList[e] == leaderboardData[f].score){
+                        arrangedData.push(leaderboardData[f]);
+                        break;
+                    }
+                }
+            }
+
+            //arrangedData is sorted leaderboard data dictionary
+
+            for (var i = 0; i < arrangedData.length; i++){
+                this.stats = this.add.text(100,240 + i * 30,`${i+1}`, {font: "26px Palatino"});
+                this.stats = this.add.text(220,240 + i * 30,`${arrangedData[i].name}`, {font: "26px Palatino"});
+                this.stats = this.add.text(430,240 + i * 30,`${arrangedData[i].score}`, {font: "26px Palatino"});
+                this.stats = this.add.text(550,240 + i * 30,`${arrangedData[i].dateOfScore.substring(0, 10)}`, {font: "18px Palatino"});
             }
             
         };
-        
     }
     
 
